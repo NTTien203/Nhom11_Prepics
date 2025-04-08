81727e363b5b884509db7b6bab91383b604a0df5
NTT203 <anhtienpro203@gmail.com>
2025-04-08 23:39:06 +0700

    add dockerfile

--- PrePics_DACN/BackEnd/src/test/java/com/example/prepics/BackendApplicationTests.java
+++ PrePics_DACN/BackEnd/src/test/java/com/example/prepics/BackendApplicationTests.java
@@ -1,13 +1,19 @@
-package com.example.prepics;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.prepics;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//
+//@SpringBootTest
+//@TestPropertySource(properties = {
+//        "cloudinary.cloud.name=test",
+//        "cloudinary.cloud.apikey=123456",
+//        "cloudinary.cloud.apisecret=abcdef"
+//})
+//class BackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
